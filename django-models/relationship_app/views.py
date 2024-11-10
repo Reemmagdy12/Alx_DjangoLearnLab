05526dae4d1d2ba8e5dbd53395053f6d5d978adb
Reemmagdy12 <110478989+Reemmagdy12@users.noreply.github.com>
2024-11-10 17:18:03 +0200

    Add files via upload

---
+++ django-models/relationship_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Book
+
+# Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    context = {'books' : books}
+    return render (request, 'relationship_app/list_books.html', context)
+
+from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+from .models import Library
+class LibraryDetail(DetailView):
+    template_name = 'relationship_app/library_detail.html'
+    model = Library
+    context_object_name = 'library'
+
+
