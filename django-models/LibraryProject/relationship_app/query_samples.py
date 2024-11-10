702c651e5be0e6cf16e7e377695b0141d85b51c0
Reemmagdy12 <110478989+Reemmagdy12@users.noreply.github.com>
2024-11-10 14:35:26 +0200

    Delete django-models/LibraryProject directory

--- django-models/LibraryProject/relationship_app/query_samples.py
+++
@@ -1,5 +0,0 @@
-from relationship_app.models import Author, Book, Library, Librarian
-Book.objects.filter(author__name="George Orwell")
-Book.objects.filter(library__name= "library")
-Librarian.objects.get(library__name="library")
-
