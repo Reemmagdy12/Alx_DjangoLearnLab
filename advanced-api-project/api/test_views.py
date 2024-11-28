20fc0fc8b49039b684af4dba19cc9649dfd4f794
Reemmagdy12 <110478989+Reemmagdy12@users.noreply.github.com>
2024-11-29 01:54:15 +0200

    Add files via upload

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient , APITestCase
+from rest_framework import status
+from .models import Book
+
+class BookTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='test', password='test123')
+        self.client.login(username = 'test', password = 'test123')
+        self.book_data = {
+            'title' : 'TestBook',
+            'author' : 'Testauthor',
+            'publication_year' : '1990'
+        }
+        self.updated_book_data = {
+            'title' : ' updatedBook',
+            'author' : 'updated',
+            'publication_year' : '1991'
+        }
+
+    def test_create_book(self):
+        response = self.client.post('api/books/', self.book_data, format = 'json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], self.book_data['title'])
+    
+    def test_update_book(self):
+        book = Book.objects.create(**self.book_data)
+        response = self.client.put('api/books/{book.id}/',self.updated_book_data, format = 'json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], self.updated_book_data['title'])
+
+    def test_delete_book(self):
+        book = Book.objects.create(**self.book_data)
+        response = self.client.delete('api/books/{book.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_permission_required(self):
+        self.client.logout()
+        response = self.client.post('api/books/', self.book_data, format = 'json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        
