ffb4ce27decee4b4632af81593cb9270d05dce19
Reemmagdy12 <110478989+Reemmagdy12@users.noreply.github.com>
2024-12-02 21:51:02 +0200

    Add files via upload

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+from .views import CreatePost, DeletePost, UpdatePost, ListPosts, PostDetails
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/',auth_views.LoginView.as_view(template_name ='registration/login.html'),name = 'login'),
+    path('register/', views.register, name ='register'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name = 'logout'),
+    path('profile/', views.profile, name='profile'),
+    path('/posts/', ListPosts.as_view(), name='posts'),
+    path('/posts/new/', CreatePost.as_view(), name='new-post'),
+    path('/posts/<int:pk>/', PostDetails.as_view(), name= 'post-details'),
+    path('/posts/<int:pk>/edit/', UpdatePost.as_view(), name='update-post'),
+    path('/posts/<int:pk>/delete/', DeletePost.as_view(), name='delete-post')
+]
