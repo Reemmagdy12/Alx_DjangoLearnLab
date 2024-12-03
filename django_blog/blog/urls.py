48bf2699b79d44bc2a8daf4cad4ea1c50e267e3f
Reemmagdy12 <110478989+Reemmagdy12@users.noreply.github.com>
2024-12-03 22:59:36 +0200

    Add files via upload

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+from .views import PostCreateView,PostDeleteView,PostDetailView,PostListView,PostUpdateView
+from .views import CommentCreateView,CommentDeleteView,CommentUpdateView,CommentListView, CommentDetailView
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/',auth_views.LoginView.as_view(template_name ='registration/login.html'),name = 'login'),
+    path('register/', views.register, name ='register'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name = 'logout'),
+    path('profile/', views.profile, name='profile'),
+    path('/posts/',PostListView.as_view(), name='posts'),
+    path('/post/new/', PostCreateView.as_view(), name='new-post'),
+    path('/posts/<int:pk>/', PostDetailView.as_view(), name= 'post-details'),
+    path('/post/<int:pk>/update/', PostUpdateView.as_view(), name='update-post'),
+    path('/post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete-post'),
+    path('/posts/<int:post_id>/comments/',CommentListView.as_view(),name = 'comments'),
+    path('/posts/<int:post_id>/comments/<int:pk>/', CommentDetailView.as_view(), name='comment_detail'),
+    path('post/<int:pk>/comments/new/',CommentCreateView.as_view(), name='new-comment'),
+    path('comment/<int:pk>/update/',CommentUpdateView.as_view(), name='update-comment'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(),name='delete-comment')
+
+]
