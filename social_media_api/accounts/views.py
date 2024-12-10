8da515558666fd7bd293eeeccc9f35f7be716f7a
Reemmagdy12 <110478989+Reemmagdy12@users.noreply.github.com>
2024-12-10 03:18:42 +0200

    Add files via upload

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,38 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.contrib.auth import authenticate
+from .serializers import UserSerializer
+from django.contrib.auth.decorators import login_required
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@login_required
+def profile(request):
+    if request.method == 'POST' :
+        user = request.user
+        user.email = request.POST.get('email', user.email)
+        user.save()
+        redirect('profile')
+    else:
+        return render (request,'profile.html',{'user':request.user})
